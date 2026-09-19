d05156466b8b5a242a375cf9952c93692471bc68
Sreypy <sreypydoeung27@gmail.com>
2026-09-19 17:39:29 +0700

    Ready for deploy

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,6 +1,7 @@
 class ApiConfig {
   static const String baseUrl = String.fromEnvironment(
     'API_URL',
-    defaultValue: 'http://localhost:3100',
+    // defaultValue: 'http://localhost:3100',
+    defaultValue: 'https://vactrackerintership-production.up.railway.app',
   );
 }
